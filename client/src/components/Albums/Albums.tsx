ab7ee91121f79f7de7d8304f0f43bd54ad057677
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-26 21:13:42 +0300

    Initial commit

--- client/src/components/Albums/Albums.tsx
+++ client/src/components/Albums/Albums.tsx
@@ -1,6 +1,21 @@
 import { useQuery } from "@tanstack/react-query"
+import { Link, useLocation } from "react-router-dom"
+import albumImg from '../../assets/album.png'
+
+interface Album{
+    userId: string,
+    id: string,
+    title: string,
+}
 
 function Albums() {
+    let location = useLocation();
+    let path = ''
+    if (location.pathname.endsWith('/')){
+        path = location.pathname.substring(0, location.pathname.length - 1)
+    } else {
+      path = location.pathname
+  }
     async function getAlbums() {
         try {
             const response = await fetch('https://jsonplaceholder.typicode.com/albums')
@@ -17,7 +32,13 @@ function Albums() {
     console.log(albums.data)
     return(
         <div>
-            <h1>Albums</h1>
+            {albums.data?.map((album : Album) => 
+            <Link to={`${path}/${album.id}`} key={album.id}>
+                <div>
+                    <img src={albumImg} alt="" />
+                    <p>{album.title}</p>
+                </div> 
+          </Link>)}
         </div>
     )
 }
