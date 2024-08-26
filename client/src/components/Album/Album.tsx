4ee20a093f8fccf22439e72e54a628a93762436e
Михаил Ладанов <MSLadanov@gmail.com>
2024-08-26 20:52:48 +0300

    Initial commit

--- client/src/components/Album/Album.tsx
+++ client/src/components/Album/Album.tsx
@@ -2,11 +2,10 @@ import { useLocation } from "react-router-dom"
 
 function Album() {
     const location = useLocation()
-    console.log(location.pathname)
     return (
         <div>
             <h1>Album</h1>
-            <h1>{location.pathname}</h1>
+            <h1>{location.pathname.split('/').at(-1)}</h1>
         </div>
     )
 }
